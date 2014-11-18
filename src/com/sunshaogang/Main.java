1619ed3e21eaf379a9bc46e96b95f02d400820ea
sunshaogang <sunshaogang@kingsoft.com>
2014-11-18 14:39:18 +0800

    init
    Signed-off-by: sunshaogang <sunshaogang@kingsoft.com>

---
+++ src/com/sunshaogang/Main.java
@@ -0,0 +1,13 @@
+package com.sunshaogang;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        sayHello();
+    }
+
+    private static void sayHello(){
+        System.out.println("Hello, World!!");
+    }
+}
